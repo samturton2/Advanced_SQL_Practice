21219f8fd83d6c3e1819a5129177b6cec0f1277e
samturton2 <STurton@spartaglobal.com>
2021-03-16 09:48:41 +0000

    create db, and first excercise complete

---
+++ Eventful_countries.sql
@@ -0,0 +1,22 @@
+Use WorldEvents
+GO
+
+select * from tblCategory;
+select * from tblContinent;
+select * from tblCountry;
+select * from tblEvent;
+
+--- Write a query which lists out countries which have more than 8 events, using a correlated subquery rather than HAVING. 
+
+SELECT cou.CountryName FROM tblCountry cou
+WHERE 8 <   (SELECT Count(ev.EventID) 
+            FROM tblEvent ev 
+            WHERE ev.CountryID = cou.CountryID)
+ORDER BY cou.CountryName ASC;
+
+
+--- Answer using the Having statement
+select distinct count(ev.EventName) as No_of_event, cou.CountryName from tblEvent ev
+inner join tblCountry cou on ev.CountryID = cou.CountryID
+group by cou.CountryName
+having count(ev.EventName) >8;
